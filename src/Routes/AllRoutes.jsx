b61e0f4d101731261ae6f702427b6f61016d4213
bakshay96 <akshaymali.edu@gmail.com>
2023-01-19 11:33:19 +0530

    router set

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,16 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
-// import Hotels from "./Hotels";
-// import SingleProduct from "./SingleProduct";
-// import CheckoutPage from "./CheckoutPage";
-// import CreateHotel from "./CreateHotal";
 import Pagenot from "../pages/Pagenot";
-// import CreateAccount from "./SignUp";
 import Login from "../pages/Login";
-// import ProfileEdit from "./ProfileEdit";
-// import ConfirmBooking from "./ConfirmBooking";
-// import Trips from "./Trips";
 const AllRoutes = () => {
   return (
     <div>
