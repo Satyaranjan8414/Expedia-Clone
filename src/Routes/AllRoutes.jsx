8f75b96e55afb8cd16708b1aea59937e92e49edc
bakshay96 <akshaymali.edu@gmail.com>
2023-01-20 19:36:43 +0530

    footer added

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -6,17 +6,18 @@ import Pagenot from "../pages/Pagenot";
 import Login from "../pages/Login";
 import CreateAccount from "../pages/SignUP";
 import Footer from "../components/Footer/Footer";
+
 const AllRoutes = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        
         
         <Route path="/login" element={<Login />} />
-        
         <Route path="/signup" element={<CreateAccount />} />
         <Route path="/footer" element={<Footer/>}/>
+       
         
         <Route path="*" element={<Pagenot />} />
         <Route path="/bookearly" element={<Bookearly/>} /> 
